e242592583f0823152418ccc9f80408725510966
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-08-27 19:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const existingBtn = document.getElementById('existing');
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const rememberMeCheckbox = document.getElementById('checkbox');
+
+    if (localStorage.getItem('username') && localStorage.getItem('password')) {
+        existingBtn.style.display = 'block';
+    }
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        if (rememberMeCheckbox.checked) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+        } else {
+            localStorage.removeItem('username');
+            localStorage.removeItem('password')
+        }
+
+        alert(`Logged in as ${username}`);
+    });
+
+    existingBtn.addEventListener('click', function() {
+        const storedUsername = localStorage.getItem('username');
+        alert(`Logged in as ${storedUsername}`);
+    });
+});
